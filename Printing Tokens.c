b383e89734dab94d26d6bb2dadc9e305170b68ee
SANTNAU GIRI <124768719+girisantanu@users.noreply.github.com>
2023-06-27 02:32:22 +0530

    Create Printing Tokens.c

---
+++ Printing Tokens.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char *s,n,i;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(*(s+i)==' ')
+        printf("\n");
+        else{
+            printf("%c",*(s+i));
+        }
+        
+        
+        
+        
+    }
+    
+   free(s);
+    
+    return 0;
+}
